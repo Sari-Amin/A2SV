5ab7093e18af315a834d75b52d54dabf1144bf55
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-05-18 20:33:07 +0300

    Created 'Car fleet' by A2SV Hub

---
+++ 18-May-2024/Car fleet 153999.py
@@ -0,0 +1,11 @@
+# Problem: Car fleet - https://leetcode.com/problems/car-fleet/
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        x = [(position[i], speed[i]) for i in range(len(speed))]
+        stack = []
+        for p,s in sorted(x)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
