336d6c48405b8529a9cf0c5b5b806231952d8add
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-05-22 22:12:14 +0300

    Created 'Broken Calculator' by A2SV Hub

---
+++ 22-May-2024/Broken Calculator 157508.py
@@ -0,0 +1,12 @@
+# Problem: Broken Calculator - https://leetcode.com/problems/broken-calculator/description/
+
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        if startValue >= target:
+            return startValue - target
+    
+        if target % 2 == 0:
+            return 1 + self.brokenCalc(startValue, target // 2)
+
+        return 1 + self.brokenCalc(startValue, target + 1)
+        
