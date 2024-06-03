84ce62c1585b863f5f60d78a3f7b571fbcb55c7a
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-06-03 22:32:53 +0300

    Created 'Three sum' by A2SV Hub

---
+++ 03-Jun-2024/Three sum 163654.py
@@ -0,0 +1,24 @@
+# Problem: Three sum - https://leetcode.com/problems/3sum/
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            left,  right = i + 1, len(nums) - 1
+
+            while left < right:
+                temp = num + nums[left] + nums[right]
+                if temp < 0:
+                    left += 1
+                elif temp > 0:
+                    right -= 1
+                else:
+                    ans.append([num, nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+        return ans
