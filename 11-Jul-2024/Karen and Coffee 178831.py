33448abf274f5e50ee2026c588ed207653e1e8c6
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-07-11 13:53:42 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 11-Jul-2024/Karen and Coffee 178831.py
@@ -0,0 +1,22 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n,k,q = map(int, input().split())
+arr = [0] * 200002
+for _ in range(n):
+    l,r = map(int, input().split())
+    arr[l] += 1
+    arr[r + 1] -= 1
+for i in range(1,200002):
+    arr[i] += arr[i - 1]
+for i in range(200002):
+    if arr[i] >= k:
+        arr[i] = 1
+    else:
+        arr[i] = 0
+
+for i in range(1,200002):
+    arr[i] += arr[i - 1]
+
+for _ in range(q):
+    l,r = map(int, input().split())
+    print(arr[r] - arr[l - 1])
