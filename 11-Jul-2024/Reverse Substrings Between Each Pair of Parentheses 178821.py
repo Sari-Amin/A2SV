2d9c6263067bf164fb2eb15a7564ebb9e1a2fb58
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-07-11 12:19:47 +0300

    Created 'Reverse Substrings Between Each Pair of Parentheses' by A2SV Hub

---
+++ 11-Jul-2024/Reverse Substrings Between Each Pair of Parentheses 178821.py
@@ -0,0 +1,18 @@
+# Problem: Reverse Substrings Between Each Pair of Parentheses - https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
+
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        ans = ""
+        stack = []
+        for ch in s:
+            if ch == ")":
+                temp = ""
+                while stack and stack[-1] != "(":
+                    temp += stack.pop()
+                if stack:
+                    stack.pop()
+                stack.extend(temp)
+            else:
+                stack.append(ch) 
+                
+        return "".join(stack)
