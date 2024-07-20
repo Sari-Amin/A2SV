aa9f565a34594010f6fdab97c88db1146bb96224
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-07-20 21:49:03 +0300

    Created 'Coorporate Flight Booking' by A2SV Hub

---
+++ 20-Jul-2024/Coorporate Flight Booking 182191.py
@@ -0,0 +1,12 @@
+# Problem: Coorporate Flight Booking - https://leetcode.com/problems/corporate-flight-bookings/
+
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        ans = [0] * n
+        for l, r, val in bookings:
+            if r != n:
+                ans[r] -= val
+            ans[l - 1] += val
+        for i in range(1,n):
+            ans[i] += ans[i - 1]
+        return ans
