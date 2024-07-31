1f483dc11c7d12eb5a46ef5a5b046956727a20e4
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-07-31 18:30:21 +0300

    Created 'Fruit Into Baskets' by A2SV Hub

---
+++ 31-Jul-2024/Fruit Into Baskets 186267.py
@@ -0,0 +1,21 @@
+# Problem: Fruit Into Baskets - https://leetcode.com/problems/fruit-into-baskets
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        freq = {}
+        l = 0
+        r = 0
+        ans = 0
+        while r < len(fruits):
+            if fruits[r] not in freq:
+                freq[fruits[r]] = 1
+            else:
+                freq[fruits[r]] += 1
+            while len(freq) > 2:
+                freq[fruits[l]] -= 1
+                if freq[fruits[l]] == 0:
+                    del freq[fruits[l]]
+                l += 1
+            ans = max(ans, r - l + 1)
+            r += 1
+        return ans
