7c8ab2cde1b25c6b366160b03d4ca46447c9b4b2
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-08-27 20:23:56 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 27-Aug-2024/New Year Transportation 208329.py
@@ -0,0 +1,13 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n,t = map(int, input().split())
+ls = list(map(int,input().split()))
+ptr = 0
+while ptr < t - 1:
+    ptr += ls[ptr]
+
+
+if ptr == t - 1:
+    print("YES")
+else:
+    print("NO")
