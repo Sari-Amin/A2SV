e57e411a3ccf970e88f4eeec57816a7de35aeffc
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-08-28 21:15:15 +0300

    Created 'String Without AAA or BBB' by A2SV Hub

---
+++ 28-Aug-2024/String Without AAA or BBB 208702.py
@@ -0,0 +1,20 @@
+# Problem: String Without AAA or BBB - https://leetcode.com/problems/string-without-aaa-or-bbb
+
+class Solution(object):
+    def strWithout3a3b(self, A, B):
+        ans = []
+
+        while A or B:
+            if len(ans) >= 2 and ans[-1] == ans[-2]:
+                writeA = ans[-1] == 'b'
+            else:
+                writeA = A >= B
+
+            if writeA:
+                A -= 1
+                ans.append('a')
+            else:
+                B -= 1
+                ans.append('b')
+
+        return "".join(ans)
