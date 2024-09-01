ac62a9920bd9329a33bc9eb1a81339d8c76d1def
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-09-01 21:16:48 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 01-Sep-2024/Group Anagrams 210167.py
@@ -0,0 +1,9 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        
+        for s in strs:
+            res["".join(sorted(s))].append(s)
+        return res.values()
