99f94aa9a45a90a7fc5c94b1e7bd2ca64f95839d
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-10-09 20:41:21 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 09-Oct-2024/Ehab Is an Odd Person 219058.py
@@ -0,0 +1,14 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())
+ls = [int(i) for i in input().split()]
+even,odd = 0,0
+for i in range(n):
+    if ls[i] % 2:
+        odd += 1
+    else:
+        even += 1
+if odd != 0 and even != 0:
+    print(*sorted(ls))
+else:
+    print(*ls)
