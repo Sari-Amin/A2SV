87c71eb7d5c8cf28a137549a15b70e60e73ec6f1
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-10-11 23:54:40 +0300

    Created 'Minimal string' by A2SV Hub

---
+++ 11-Oct-2024/Minimal string 219615.py
@@ -0,0 +1,20 @@
+# Problem: Minimal string - https://codeforces.com/contest/797/problem/C
+
+from collections import Counter
+s = input()
+dict = Counter(s)
+t,u = [],[]
+
+for ch in s:
+    t.append(ch)
+    dict[ch]-=1
+    if dict[ch]==0:
+        del dict[ch]
+        
+    while t and (not dict or min(dict) >= t[-1]):
+        u.append(t.pop())
+        
+print(''.join(u))
+
+
+
