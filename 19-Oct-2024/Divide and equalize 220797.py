0bdb5377c520410d616d965035f14bfd2bbf7822
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-10-19 22:47:39 +0300

    Created 'Divide and equalize' by A2SV Hub

---
+++ 19-Oct-2024/Divide and equalize 220797.py
@@ -0,0 +1,31 @@
+# Problem: Divide and equalize - https://codeforces.com/problemset/problem/1881/D
+
+from collections import defaultdict
+
+
+def add_divs(x, divs):
+    i = 2
+    while i * i <= x:
+        while x % i == 0:
+            divs[i] += 1
+            x //= i
+        i += 1
+    if x > 1:
+        divs[x] += 1
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split()))
+    divs = defaultdict(int)
+    for i in range(n):
+        add_divs(a[i], divs)
+    for e in divs.values():
+        if e % n != 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        print("YES" if solve() else "NO")
+
