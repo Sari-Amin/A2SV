20f8d0ec9ddafcc982445025fff418af66ce9d92
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-11-16 21:04:05 +0300

    Created 'Valid Palindrome' by A2SV Hub

---
+++ 16-Nov-2024/Valid Palindrome 224033.py
@@ -0,0 +1,9 @@
+# Problem: Valid Palindrome - https://leetcode.com/problems/valid-palindrome/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ""
+        for ch in s:
+            if ch.isalnum():
+                new_s += ch.lower()
+        return new_s == new_s[::-1]
