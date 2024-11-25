ea132877aec9bad1b89ae7955f2142b419f77912
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-11-25 21:27:00 +0300

    Created 'Cover in Water' by A2SV Hub

---
+++ 25-Nov-2024/Cover in Water 224329.py
@@ -0,0 +1,27 @@
+# Problem: Cover in Water - https://codeforces.com/problemset/problem/1900/A
+
+for _ in range(int(input())):
+    n = int(input())
+    s = input()
+    left = s.find(".")
+    ans = 0
+    right = left
+    pos = False
+    while right < n:
+        if s[right] == ".":
+            right += 1
+        else:
+            if right - left  >= 3:
+                pos = True
+            else:
+                ans += right - left
+            right += 1
+            left = right
+    if right - left  >= 3:
+        pos = True
+    else:
+        ans += right - left  
+    if pos:
+        print(2)
+    else:
+        print(ans)
