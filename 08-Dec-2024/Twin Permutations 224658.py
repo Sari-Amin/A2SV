fb345dbe2046b5377cef41afd49e2b31d0b42ac3
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-12-08 21:07:59 +0300

    Created 'Twin Permutations' by A2SV Hub

---
+++ 08-Dec-2024/Twin Permutations 224658.py
@@ -0,0 +1,11 @@
+# Problem: Twin Permutations - https://codeforces.com/contest/1831/problem/A
+
+for _ in range(int(input())):
+    n = int(input())
+    ls = list(map(int, input().split()))
+    ans = sorted(ls)
+    for i in range(n):
+        ans[i] = n + 1 - ls[i]
+        
+    print(*ans)
+        
