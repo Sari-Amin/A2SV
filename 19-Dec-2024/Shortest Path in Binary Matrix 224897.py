d97c8ae90a302c95ef95d21c7a6737dcae584e9d
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2024-12-20 01:06:50 +0300

    Created 'Shortest Path in Binary Matrix' by A2SV Hub

---
+++ 19-Dec-2024/Shortest Path in Binary Matrix 224897.py
@@ -0,0 +1,33 @@
+# Problem: Shortest Path in Binary Matrix - https://leetcode.com/problems/shortest-path-in-binary-matrix/description/
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        res = -1
+    
+        directions =[(-1,0),(1,0),(0,1),(0,-1),(-1,-1),(1,1),(-1,1),(1,-1)]
+
+
+        def inbound(i,j):
+            return i>=0 and i < n and j >= 0 and j < n
+
+        if grid[0][0] == 1:
+            return res
+        que = deque([(0,0)])
+        level = 1
+        get = False
+        while que and not get:
+            for _ in range(len(que)):
+                i,j = que.popleft()
+                if (i, j) == (n - 1, n - 1):
+                    res = level
+                    get = True
+                    break
+                for r,c in directions:
+                    nr = i + r
+                    nc = j + c
+                    if inbound(nr,nc) and grid[nr][nc] != 1:
+                        que.append((nr,nc))
+                        grid[nr][nc] = 1
+            level += 1
+        return res
