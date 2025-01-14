3a84bab2b3e78e124a41f796143ddf9ea4246b18
Sari Amin <58248593+Sari-Amin@users.noreply.github.com>
2025-01-14 12:45:15 +0300

    Created 'Segment with Small Sum' by A2SV Hub

---
+++ 14-Jan-2025/Segment with Small Sum 225460.py
@@ -0,0 +1,21 @@
+# Problem: Segment with Small Sum - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+
+n, s = map(int, input().split())
+ls = list(map(int, input().split()))
+
+left = 0
+right = 0
+ans = 0
+sm = 0
+while right < n:
+    if sm + ls[right] <= s:
+        sm += ls[right]
+        right += 1
+    else:
+        ans = max(ans, right - left)
+        sm -= ls[left]
+        left += 1
+
+
+ans = max(ans, right - left)
+print(ans)
